fac972b6bd45211d3e42293d182f3d335e3402aa
Jardel Bordignon <jardel1101@gmail.com>
2024-08-23 22:40:27 -0300

    :sparkles: service-worker

---
+++ src/service-worker/sw.ts
@@ -0,0 +1,24 @@
+/// <reference lib="WebWorker" />
+
+import {
+  cleanupOutdatedCaches,
+  createHandlerBoundToURL,
+  precacheAndRoute,
+} from "workbox-precaching";
+import { clientsClaim } from "workbox-core";
+import { NavigationRoute, registerRoute } from "workbox-routing";
+
+declare let self: ServiceWorkerGlobalScope;
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+cleanupOutdatedCaches();
+
+let allowlist: undefined | RegExp[];
+
+if (import.meta.env.DEV) allowlist = [/^\/$/];
+
+registerRoute(new NavigationRoute(createHandlerBoundToURL("/"), { allowlist }));
+
+self.skipWaiting();
+clientsClaim();
